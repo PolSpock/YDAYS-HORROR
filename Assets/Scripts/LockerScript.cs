fa0d9da490f87b97ed7aed6dd341d2c6ec14f543
PolSpock <chao-garden@hotmail.com>
2019-02-20 21:26:21 +0100

    Création folder script + 2 scripts vierge

---
+++ Assets/Scripts/LockerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            print("up arrow key is held down");
+        }
+
+        if (Input.GetKey("down"))
+        {
+            print("down arrow key is held down");
+        }
+    }
+}
