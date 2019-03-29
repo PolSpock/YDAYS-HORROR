69eb226229dfa52da655953360b9eca99a009ee2
arthur <arthur.genthial@ynov.com>
2019-03-29 16:29:08 +0100

    fille inviible

--- Assets/Scripts/walk.cs
+++ Assets/Scripts/walk.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class walk : MonoBehaviour
 {
+   
     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,7 +17,6 @@ public class walk : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime);
 
         // Move the object upward in world space 1 unit/second.
-       // transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        // transform.Translate(Vector3.up * Time.deltaTime, Space.World);
     }
-
 }
